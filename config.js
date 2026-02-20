bdc04437c4f58d53d61fb0821dcc09a476fe9fac
Alcariss <jan.chmelar@outlook.com>
2026-02-20 22:25:29 +0100

    Restore personal config

--- config.js
+++ config.js
@@ -3,16 +3,16 @@
 
 const APP_CONFIG = {
   // Google Apps Script deployment URL
-  SCRIPT_URL: 'YOUR_GOOGLE_APPS_SCRIPT_URL',
+  SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbyFbg-AvWiSpBGKOgBAJkp1uA9o-qnPA17oTLuPJ29aEWzD6wEYQN7jnA-2jdHrsTEMbQ/exec',
   
   // Google Sheet ID (from the sheet URL)
-  SHEET_ID: 'YOUR_GOOGLE_SHEET_ID',
+  SHEET_ID: '1eXW41Uvwm8TqIGFhhHPSbNhQpObnEwM0PwV707dYU-A',
   
   // Child's name (used to form app title, e.g., "Hanu" -> "Hanuhlášky")
-  KID_NAME: 'YourKid',
+  KID_NAME: 'Hanu',
   
   // Emoji displayed with the title
-  EMOJI: '⭐',
+  EMOJI: '🍯',
   
   // App timeouts (in milliseconds)
   TIMEOUT: 8000,
